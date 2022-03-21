6a94d53efe5f3fb88f7329b8a1c06fcd6ec65711
Lejimin <wldms025747@naver.com>
2022-03-21 17:31:02 +0900

    2022-03-21-2

---
+++ src/Model/BoardDAO.java
@@ -0,0 +1,41 @@
+package Model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class BoardDAO {
+
+	Connection con;
+	PreparedStatement pstmt;
+	ResultSet rs;
+
+	
+	public void getCon() {
+		
+		try {
+			Context initctx = new InitialContext();
+			
+			Context envctx = (Context) initctx.lookup("java:comp/env");
+			DataSource ds = (DataSource) envctx.lookup("jdbc/mysql");
+			
+			con = ds.getConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
